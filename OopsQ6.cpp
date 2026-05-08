231e90068bd3dbbb9d184bbad8f85e63a49f93a5
Dakshkawatra <kawatradaksh1@gmail.com>
2026-05-08 22:03:24 +0530

    Temperature Converter

---
+++ OopsQ6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Temperature {
+private:
+    float celsius;
+
+public:
+    void inputTemperature() {
+        cout << "Enter Temperature in Celsius: ";
+        cin >> celsius;
+    }
+
+    float convertToFahrenheit() {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    void displayResult() {
+        cout << "\nTemperature in Fahrenheit: "
+             << convertToFahrenheit() << endl;
+    }
+};
+
+int main() {
+    Temperature t;
+    t.inputTemperature();
+    t.displayResult();
+
+    return 0;
+}
